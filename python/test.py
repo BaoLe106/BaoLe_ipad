d325c4b5df0d818d2b5a43c0db551446b6dca444
Bao Le <127121163+BaoLe106@users.noreply.github.com>
2023-05-03 09:49:09 +0800

    lastest commit

--- python/test.py
+++ python/test.py
@@ -1,1 +1,21 @@
-print('Hello world')
+import random
+import threading
+
+global E 
+E = 100
+global x, y
+def func1():
+    while E > 1e-2:
+        x = random.random()
+        y = random.random()
+        
+def func2():
+    while E > 1e-2:
+        E = x**2 + y**2
+        print(x, y, E)
+        
+t1 = threading.Thread(target = func1)
+t2 = threading.Thread(target = func2)
+
+t1.start()
+t2.start()
